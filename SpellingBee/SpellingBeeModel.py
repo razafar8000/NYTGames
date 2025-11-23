592d8b1d37e8eb7e2120a1eba6aebc376bb6d9fd
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2025-11-23 12:19:54 -0800

    refactor: corrected return type for containsWord

--- SpellingBee/SpellingBeeModel.py
+++ SpellingBee/SpellingBeeModel.py
@@ -6,47 +6,58 @@ class SpellingBeeModel:
     def __init__(self):
         self.__points = 0 
         self.__validAnswers = []
-        self.__validLetters = ""
+        self.__usableLetters = []
         WordListGenerator = reader.WordDictionary("WordRepository\words_dictionary.json")
         self.__wordList = WordListGenerator.getWordsOfLength(7)
-        self.generateValidLetters(self)
+        self.generateUsableLetters()
 
     #checks to see if userInput contains the 7 selected words
 
-    #Generates list of 7 random characters, at least two to three vowels and 5 constants
-    def generateValidLetters(self):
+    #creates a random list of 7 characters that contains letters that are able to be used in spelling bee
+    def generateUsableLetters(self):
         vowels = "aeiou"
         consonants = "bcdfghjklmnpqrstvwxyz"
         maxInput = 7
+
         chosenVowelCount = random.randint(1,3)
         chosenConsonantCount = maxInput - chosenVowelCount
-        newValidAnswers = []
+        newUsableLetters = ""
+
+        #chooses random index in vowels and consonant string
         for x in range(chosenVowelCount):
-            newValidAnswers.append(vowels[random.randint(len(vowels)-1)])
+            randomIndex = random.randint(len(vowels)-1)
+            newUsableLetters += vowels[randomIndex]
         for x in range(chosenConsonantCount):
-            newValidAnswers.append(consonants[random.randint(len(consonants)-1)])
-            
+            randomIndex = random.randint(len(vowels)-1)
+            newUsableLetters += consonants[randomIndex]
+
+        self.__usableLetters = newUsableLetters
+        return newUsableLetters
+    
+
     def hasValidLetters(self, userInput):
         userInput = list(userInput)
         for letter in userInput:
-            if letter not in self.__validLetters:
+            if letter not in self.__usableLetters:
                 return False
         return True
-    #gets list of letter that are invalid to respond to user
-    def getInvalidLetters(self,userInput):
-        userInput = str(list(userInput))
+    
+    #gets list of letter that are invalid to input (for UI purposes)
+    def getInvalidLetters(self, buffer):
+        bufferList = str(list(buffer))
         invalidLetterList = []
-        for letter in userInput:
-            if letter not in self.__validLetters:
+        for letter in bufferList:
+            if letter not in self.__usableLetters:
                 invalidLetterList.append[letter]
-        invalidLetterList
+        return invalidLetterList
         
     #If word found in list of words length 7, return true
     def containsWord(self, userInput):
         if userInput in self.__wordList:
-            return False
-        else:
             return True
+        else:
+            return False
+
     def addPoint(self):
         self.__points += 1
     def getPoints(self):
